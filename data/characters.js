cda6908f082858ef070dac074012b1a4b2341ef1
springthree1442 <green.oliveade@gmail.com>
2026-06-27 23:04:25 +0900

    Add character definitions to characters.js

---
+++ data/characters.js
@@ -0,0 +1,46 @@
+const CHARACTERS = {
+  narrator: {
+    name: "",
+    color: "#f2f4f8"
+  },
+
+  player: {
+    name: "P",
+    color: "#f2f4f8"
+  },
+
+  unknown: {
+    name: "???",
+    color: "#aab0c0"
+  },
+
+  z: {
+    name: "사수 z",
+    color: "#d7d7e8"
+  },
+
+  flot: {
+    name: "플롯",
+    color: "#d7b36a"
+  },
+
+  swan: {
+    name: "백조",
+    color: "#d85f5f"
+  },
+
+  dealerB: {
+    name: "테이블 B의 딜러",
+    color: "#b8c7ff"
+  },
+
+  loudMan: {
+    name: "시끄러운 무리의 남성",
+    color: "#c9c9c9"
+  },
+
+  loudWoman: {
+    name: "소란스러운 무리의 여성",
+    color: "#c9c9c9"
+  }
+};
