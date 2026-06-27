25af0309dfe9ffa05a84f849fd2d2021532d99f9
springthree1442 <green.oliveade@gmail.com>
2026-06-27 23:17:40 +0900

    Update chapter1.js

--- data/chapter1.js
+++ data/chapter1.js
@@ -1,30 +1,300 @@
 const CHAPTER_1 = [
   {
+    id: "ch1_title",
+    type: "title",
+    text: "Act.1 균열\nChapter.1 조명이 밝은 밤"
+  },
+
+  {
     id: "ch1_001",
     type: "narration",
     text: "어둠이 내려앉은 거리."
   },
+
   {
     id: "ch1_002",
     type: "narration",
     text: "저물어가는 하루를 마무리 하기 위해 사람들은 짙은 어둠 속으로 모인다."
   },
+
   {
-    id: "ch1_z_001",
+    id: "ch1_003",
+    type: "narration",
+    text: "인파가 늘어남에 따라 뒷골목의 네온사인이 하나 둘 모습을 드러내고,"
+  },
+
+  {
+    id: "ch1_004",
+    type: "narration",
+    text: "어쩌면 정오보다 밝을 밤의 거리가 두 번째 오늘을 맞이하도록 금세 온 건물의 조명을 태운다."
+  },
+
+  {
+    id: "ch1_005",
+    type: "narration",
+    text: "여기는 조직 『새벽』의 여러 사업장이 자리 잡은 거리, 《Grand Vine》."
+  },
+
+  {
+    id: "ch1_006",
+    type: "narration",
+    text: "그랜드 바인 중심에 자리잡은 밤의 명소 《Gambit Rouge》는 단연코 이 거리에서 가장 인기있는 와인바이다."
+  },
+
+  {
+    id: "ch1_007",
+    type: "narration",
+    text: "당신 또한 처음으로 이 바닥에 발을 들인다면 홀린 듯 중앙 광장의 갬빗 루즈를 찾게 될 것이며,"
+  },
+
+  {
+    id: "ch1_008",
+    type: "narration",
+    text: "우리 갬빗 루즈는 언제나 당신에게 최고로 화려한 밤을 선사하리라."
+  },
+
+  {
+    id: "ch1_009",
+    type: "narration",
+    text: "갬빗 루즈는 1층과 2층, 그리고 지하까지 모두 다섯 개 층으로 이루어져 있다."
+  },
+
+  {
+    id: "ch1_010",
+    type: "narration",
+    text: "지상층은 평범한 대형 와인바와 크게 다르지 않다."
+  },
+
+  {
+    id: "ch1_011",
+    type: "narration",
+    text: "바텐더석 뒤편, 커튼으로 가려진 좁은 통로를 지나면 지하로 이어지는 계단과 엘리베이터가 나온다."
+  },
+
+  {
+    id: "ch1_012",
+    type: "narration",
+    text: "지하 1층에 다다라야 비로소 갬빗 루즈의 진짜 모습이 드러난다."
+  },
+
+  {
+    id: "ch1_013",
+    type: "narration",
+    text: "‘나’는 이곳의 지하 2층 보조 매니저로 일하고 있는 평범한 30대 초반."
+  },
+
+  {
+    id: "ch1_014",
+    type: "narration",
+    text: "올해로 『새벽』에 몸을 담근 지 4년째, 담당 업무가 사업장 관리 보조로 바뀐지는 1년도 채 되지 않았다."
+  },
+
+  {
+    id: "ch1_015",
+    type: "narration",
+    text: "사수와 함께 갬빗 루즈를 맡기 전까진 현장처리반 소속 지휘관이었다."
+  },
+
+  {
+    id: "ch1_016",
+    type: "narration",
+    text: "남들보다 비교적 편하게 올라올 수 있었던 건 운이 좋게도 유능한 사수를 만난 덕이지."
+  },
+
+  {
+    id: "ch1_017",
     type: "dialogue",
     speaker: "unknown",
     text: "{player}."
   },
+
   {
-    id: "ch1_p_001",
+    id: "ch1_018",
+    type: "narration",
+    text: "부르는 소리에 응하여 고개를 돌리면, 백금발 머릿결을 양쪽으로 넘긴 반묶음의 남자가 흑색 눈동자로 ‘나’를 응시하고 있다."
+  },
+
+  {
+    id: "ch1_019",
     type: "dialogue",
     speaker: "player",
     text: "오셨어요, 선배."
   },
+
   {
-    id: "ch1_z_002",
+    id: "ch1_020",
+    type: "narration",
+    text: "사수 z는 이곳 갬빗 루즈의 총책임 관리자이며 다음 달 쯤이면 간부 자리로 올라갈 기미가 보일 정도로 조직 내에서 꽤 중요한 자리를 맡고 있다."
+  },
+
+  {
+    id: "ch1_021",
     type: "dialogue",
     speaker: "z",
     text: "지하 2층을 너한테 맡긴 건 정말 잘한 일이야. 애로사항 같은 건 없지?"
+  },
+
+  {
+    id: "ch1_022",
+    type: "dialogue",
+    speaker: "player",
+    text: "별말씀을요. 선배 덕이 큰데. 오늘도 평소랑 비슷해요."
+  },
+
+  {
+    id: "ch1_023",
+    type: "narration",
+    text: "그는 내가 아주 신입일 때부터도 주변에 박하게 굴지 않고 후임들에게 잘 해주어서 평판도 좋았다."
+  },
+
+  {
+    id: "ch1_024",
+    type: "dialogue",
+    speaker: "z",
+    text: "오늘 자정, 기억해."
+  },
+
+  {
+    id: "ch1_025",
+    type: "dialogue",
+    speaker: "player",
+    text: "…"
+  },
+
+  {
+    id: "ch1_026",
+    type: "narration",
+    text: "어떤 서류를 전달 받아야 한다고 했던가."
+  },
+
+  {
+    id: "ch1_027",
+    type: "narration",
+    text: "중요한 임무를 내게 맡긴 건 퍽 기꺼웠지만, 전산에도 남기지 않는 원본을 직접 전달받는 일은 처음이었다."
+  },
+
+  {
+    id: "ch1_028",
+    type: "dialogue",
+    speaker: "z",
+    text: "테이블 B에 잠깐 딜러가 자리를 비울거라 네가 한 게임만 맡아줘."
+  },
+
+  {
+    id: "ch1_029",
+    type: "dialogue",
+    speaker: "z",
+    text: "그 다음에 이동하면 시간이랑 동선 여유있게 맞을거야."
+  },
+
+  {
+    id: "ch1_030",
+    type: "task",
+    text: "【목록】이 갱신되었습니다."
+  },
+
+  {
+    id: "ch1_031",
+    type: "dialogue",
+    speaker: "z",
+    text: "…"
+  },
+
+  {
+    id: "ch1_032",
+    type: "dialogue",
+    speaker: "z",
+    text: "꼭 ‘그 사람’도 데려와."
+  },
+
+  {
+    id: "ch1_choice_001",
+    type: "choice",
+    text: "어떻게 대답할까?",
+    choices: [
+      {
+        label: "알겠다고 대답한다.",
+        result: [
+          {
+            id: "ch1_choice_001_a_001",
+            type: "dialogue",
+            speaker: "player",
+            text: "물론이죠. 맡겨주세요."
+          },
+          {
+            id: "ch1_choice_001_a_002",
+            type: "dialogue",
+            speaker: "z",
+            text: "그럼, 믿을게."
+          }
+        ]
+      },
+      {
+        label: "그 사람이 누구인지 묻는다.",
+        result: [
+          {
+            id: "ch1_choice_001_b_001",
+            type: "dialogue",
+            speaker: "player",
+            text: "그 사람, 이라면…?"
+          },
+          {
+            id: "ch1_choice_001_b_002",
+            type: "dialogue",
+            speaker: "z",
+            text: "장부 가져올 사람 말이야. 아는 사이니까 어렵지 않지?"
+          }
+        ]
+      },
+      {
+        label: "아무 대답도 하지 않는다.",
+        result: [
+          {
+            id: "ch1_choice_001_c_001",
+            type: "dialogue",
+            speaker: "player",
+            text: "……"
+          },
+          {
+            id: "ch1_choice_001_c_002",
+            type: "dialogue",
+            speaker: "z",
+            text: "긴장했어? 나쁘진 않네. 실수하면 안 되니까."
+          }
+        ]
+      }
+    ]
+  },
+
+  {
+    id: "ch1_033",
+    type: "dialogue",
+    speaker: "unknown",
+    text: "얼룩말! 이쪽 한 번만 와주세요…!"
+  },
+
+  {
+    id: "ch1_034",
+    type: "narration",
+    text: "그때, 지하 1층의 메인홀 딜러가 그를 급히 불렀다."
+  },
+
+  {
+    id: "ch1_035",
+    type: "dialogue",
+    speaker: "z",
+    text: "가봐야겠다. 지하 3층 룸에서 봐."
+  },
+
+  {
+    id: "ch1_036",
+    type: "narration",
+    text: "그는 목소리가 부른 쪽으로 향했다."
+  },
+
+  {
+    id: "ch1_037",
+    type: "narration",
+    text: "[ 테이블 B로 가자. ]"
   }
 ];
