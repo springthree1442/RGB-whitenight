cd7e467b986f95ae1988526fcfd202fedbc544b5
springthree1442 <green.oliveade@gmail.com>
2026-06-27 23:05:58 +0900

    Create chapter1.js

---
+++ data/chapter1.js
@@ -0,0 +1,30 @@
+const CHAPTER_1 = [
+  {
+    id: "ch1_001",
+    type: "narration",
+    text: "어둠이 내려앉은 거리."
+  },
+  {
+    id: "ch1_002",
+    type: "narration",
+    text: "저물어가는 하루를 마무리 하기 위해 사람들은 짙은 어둠 속으로 모인다."
+  },
+  {
+    id: "ch1_z_001",
+    type: "dialogue",
+    speaker: "unknown",
+    text: "{player}."
+  },
+  {
+    id: "ch1_p_001",
+    type: "dialogue",
+    speaker: "player",
+    text: "오셨어요, 선배."
+  },
+  {
+    id: "ch1_z_002",
+    type: "dialogue",
+    speaker: "z",
+    text: "지하 2층을 너한테 맡긴 건 정말 잘한 일이야. 애로사항 같은 건 없지?"
+  }
+];
